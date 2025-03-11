d139edace4323d8e14fc679fedca11d28128f17b
pengji-king <pengji.24@intl.zju.edu.cn>
2025-03-11 09:23:00 +0800

    preview for practical 4

---
+++ Practical4/boolean.py
@@ -0,0 +1,5 @@
+X = True
+Y = False
+Z = X or Y
+print(Z)
+#if X and Y outcome is False if it is X oy Y the out come is True
---
+++ Practical4/trinub.py
@@ -0,0 +1,4 @@
+trinub = 0
+for i in range(1,11):
+    trinub = trinub + i
+    print(trinub)
