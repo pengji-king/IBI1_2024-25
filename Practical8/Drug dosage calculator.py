65c56a38a1e11cb70477f1c9d027c61b0446ab67
pengji-king <pengji.24@intl.zju.edu.cn>
2025-04-08 19:03:29 +0800

    preview practical 8

---
+++ Practical8/Drug dosage calculator.py
@@ -0,0 +1,15 @@
+
+# i need write a function which take two input of body weight and strength of paracetamol 
+def drug_culculator (weight, strength):
+    if 10<weight<100 and strength == 120:
+        volume = 5*15*weight/120
+    elif 10<weight<100 and strength == 250:
+        volume = 5*15*weight/250
+    else:
+        print("error")
+    return volume
+
+volume = drug_culculator(80,120)
+print(volume)
+
+
---
+++ Practical8/Patient record management.py
@@ -0,0 +1,17 @@
+#create a class called patient
+class patients:
+    def __init__(self, name, age, date, history):
+        self.name = name
+        self.age = age
+        self.date = date
+        self.history = history
+    
+    def describle (self):
+        info = self.name + " ages " + str(self.age)+" burn in "+ self.date + " " + self.history
+        print(info)
+        return info
+
+patient1 = patients("Jay",18, "2025.03.12","no medicine history")
+patients.describle(patient1)
+        
+    
